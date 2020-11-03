c159ed2ed4502b6540e5004fa8dd8df5d541c7e8
Luke Gessler <lukegessler@gmail.com>
2020-11-03 13:50:54 -0500

    write precisions at k instead

--- score.py
+++ score.py
@@ -1,69 +1,64 @@
 import argparse
-import csv
+from tqdm import tqdm
 from collections import defaultdict
-
 import pandas as pd
-import re
+from ldg.pickle import pickle_read, pickle_write
 
 N = 50
 
 
-def score(tsv_path):
-    at_least_one_pos_counts = defaultdict(lambda: {"label": 0, "synset": 0, "lemma": 0, "total": 0})
-    at_least_one_label = 0
-    at_least_one_synset = 0
-    at_least_one_lemma = 0
-    no_pos_tag = 0
+def precisions_at_k(df, pickle_path=None, min_train_freq=5):
+    if pickle_path is not None:
+        data = pickle_read(pickle_path)
+        if data is not None:
+            return data
+    # k -> name of exact metric (incl. total) -> count
+    # Dict[int, Dict[str, int]]
 
-    df = pd.read_csv(tsv_path, sep="\t")
-    for i, row in df.iterrows():
-        retrieved_labels = [getattr(row, f'label_{i+1}') for i in range(N)]
-        retrieved_synsets = [getattr(row, f'synset_{i+1}') for i in range(N)]
-        retrieved_lemmas = [getattr(row, f'lemma_{i+1}') for i in range(N)]
+    # for pickles
+    def f1():
+        def f2():
+            return 0
+        return defaultdict(f2)
+    correct_at_k = defaultdict(f1)
+    
+    for _, row in tqdm(df.iterrows()):
+        label = row.label
+        synset = row.synset
+        lemma = row.lemma
+        if row.label_freq_in_train < min_train_freq:
+            continue
+        
+        for k in range(1, N+1):
+            labels = [getattr(row, f"label_{i}") for i in range(1, k+1)]
+            synsets = [getattr(row, f"synset_{i}") for i in range(1, k+1)]
+            lemmas = [getattr(row, f"lemma_{i}") for i in range(1, k+1)]
+            correct_at_k[k]['label'] +=  len([x for x in labels if x == label])
+            correct_at_k[k]['synset'] += len([x for x in synsets if x == synset])
+            correct_at_k[k]['lemma'] +=  len([x for x in lemmas if x == lemma])
+            correct_at_k[k]['total'] += k
 
-        try:
-            pos = re.findall(r"\.(.*)\.", row.label)[0]
-        except:
-            no_pos_tag += 1
-            assert row.label == "NE"
-            pos = "NE"
-        #print(row.sentence)
-        #print(row.label, retrieved_labels)
-        #print(row.synset, retrieved_synsets)
-        #print(row.lemma, retrieved_lemmas)
-        #print()
-        at_least_one_pos_counts[pos]["total"] += 1
+    ps_at_k = defaultdict(lambda: dict())
+    for k in range(1, N+1):
+        for l in ['label', 'synset', 'lemma']:
+            ps_at_k[k][l] = correct_at_k[k][l] / correct_at_k[k]['total'] 
 
-        if any(row.label == label for label in retrieved_labels):
-            at_least_one_label += 1
-            at_least_one_pos_counts[pos]['label'] += 1
-        if any(row.synset == synset for synset in retrieved_synsets):
-            at_least_one_synset += 1
-            at_least_one_pos_counts[pos]['synset'] += 1
-        if any(row.lemma == lemma for lemma in retrieved_lemmas):
-            at_least_one_lemma += 1
-            at_least_one_pos_counts[pos]['lemma'] += 1
-
-    n = len(df.index)
-    print(f"at least one label:  {at_least_one_label / n}")
-    print(f"at least one synset: {at_least_one_synset / n}")
-    print(f"at least one lemma:  {at_least_one_lemma / n}")
-    print(f"no pos tag:          {no_pos_tag} ({no_pos_tag / n * 100}%)")
-
-    for pos, dist in at_least_one_pos_counts.items():
-        n = dist['total']
-        print()
-        print(f"pos: {pos}")
-        print(f"at least one label:  {dist['label'] / n}")
-        print(f"at least one synset: {dist['synset'] / n}")
-        print(f"at least one lemma:  {dist['lemma'] / n}")
+    if pickle_path is not None:
+        ps_at_k = dict(ps_at_k)
+        for key, value in ps_at_k.items():
+            ps_at_k[key] = dict(value)
+        pickle_write(ps_at_k, pickle_path)
+    return ps_at_k
 
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("predictions_tsv")
     args = p.parse_args()
-    score(args.predictions_tsv)
+
+    df = pd.read_csv(args.predictions_tsv, sep="\t")
+    for train_freq in [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]:
+        precisions_at_k(df, pickle_path=args.predictions_tsv + f'.{train_freq}.pkl', min_train_freq=train_freq)
 
 
 if __name__ == '__main__':
