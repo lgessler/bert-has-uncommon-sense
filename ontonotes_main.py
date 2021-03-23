85e8b6459a141d86cd69767391838e954d8b83a1
Luke Gessler <lukegessler@gmail.com>
2021-03-22 23:14:23 -0400

    bad line handling

--- ontonotes_main.py
+++ ontonotes_main.py
@@ -207,7 +207,11 @@ def main(embedding_name, distance_metric, pos='all', query_category='all', top_n
     with open('cache/ontonotes_stats/train_lemma_freq.tsv', 'r') as f:
         lemma_freqs = {k: int(v) for k, v in map(lambda l: l.strip().split('\t'), f)}
 
-    df = pd.read_csv(paths.predictions_tsv_path(distance_metric, embedding_name, query_n, bert_layers=bert_layers), sep='\t')
+    df = pd.read_csv(
+        paths.predictions_tsv_path(distance_metric, embedding_name, query_n, bert_layers=bert_layers),
+        sep='\t',
+        error_bad_lines=False
+    )
     for min_train_freq, max_train_freq in TRAIN_FREQ_BUCKETS:
         for min_rarity, max_rarity in PREVALENCE_BUCKETS:
             print(f"Cutoff: [{min_train_freq},{max_train_freq}), Rarity: [{min_rarity},{max_rarity})")
